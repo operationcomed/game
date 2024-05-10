bbe611cdf6153957408a8cae9742261b2e0ef934
steph <69134282+dsbure@users.noreply.github.com>
2024-05-10 09:39:50 +0800

    uwu

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Escape MSU (Mindanao State University, not the one in the US)
 # the coding skill differential from when this project started to now is really large
 # >50% of this is what i'd consider bad code now
+# blame me for all the bad code :)
+#hi! this is david ><uwu im so tired 
 import game
 
 app = game.Game()
